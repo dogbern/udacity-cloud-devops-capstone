001947f8b31b047d76f2922414ac33950f336a0b
dogbern <dogbern@gmail.com>
2020-08-11 01:29:51 -0500

    fixed pylint issues

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_udacity():
-	return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
